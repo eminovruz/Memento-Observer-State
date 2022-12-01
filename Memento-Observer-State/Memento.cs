1312d02dcda6157862ad02f3ad064c02b25f0a57
Novr_wz05 <Novr_wz05@STHQ0125-20>
2022-12-01 19:19:20 +0400

    Add project files.

---
+++ Memento-Observer-State/Memento.cs
@@ -0,0 +1,69 @@
+﻿#nullable disable
+
+class TabMemento
+{
+    public string Name { get; set; }
+    public byte Size { get; set; }
+}
+
+class TabOriginator
+{
+    public string Name { get; set; }
+    public byte Size { get; set; }
+
+    internal TabUndoCareTaker TabUndoCareTaker
+    {
+        get => default;
+        set
+        {
+        }
+    }
+
+    private TabUndoCareTaker _tabCareTaker;
+
+    public TabOriginator()
+    {
+        _tabCareTaker = new TabUndoCareTaker();
+    }
+
+    public void Save()
+    {
+        _tabCareTaker.TextMemento = new TabMemento
+        {
+            Name = this.Name,
+            Size = this.Size
+        };
+    }
+
+    public void Undo()
+    {
+        TabMemento previousTextMemento = _tabCareTaker.TextMemento;
+
+        Name = previousTextMemento.Name;
+        Size = previousTextMemento.Size;
+    }
+    public override string ToString()
+        => $"Name: {Name}, Size: {Size.ToString()}";
+}
+
+class TabUndoCareTaker
+{
+    private Stack<TabMemento> _mementos;
+
+    public TabUndoCareTaker()
+    {
+        _mementos = new Stack<TabMemento>();
+    }
+
+    public TabMemento TextMemento
+    {
+        get
+        {
+            return _mementos.Pop();
+        }
+        set
+        {
+            _mementos.Push(value);
+        }
+    }
+}
---
+++ Memento-Observer-State/State.cs
@@ -0,0 +1,62 @@
+﻿class Document
+{
+    IState state;
+
+    public void Render()
+    {
+        state.Render();
+    }
+
+    public void Publish()
+    {
+        state.Publish();
+    }
+
+    public void ChangeState(IState State)
+    {
+        state = State;
+    }
+}
+
+interface IState
+{
+    void Render();
+
+    void Publish();
+}
+
+class Draft : IState
+{
+    private Document _document;
+
+    public void ChangeState()
+        => _document.ChangeState(new Rar());
+    
+    public void Publish()
+    {
+        // Some Codes...
+    }
+
+    public void Render()
+    {
+        // Some Codes...
+    }
+}
+
+class Rar : IState
+{
+    private Document _document;
+
+    public void ChangeState()
+        => _document.ChangeState(new Draft());
+
+    public void Publish()
+    {
+        // Some Codes...
+    }
+
+    public void Render()
+    {
+        // Some Codes...
+    }
+}
